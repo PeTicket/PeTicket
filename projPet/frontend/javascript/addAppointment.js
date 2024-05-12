285eddcf46fab2963f9d8c9b923a67dc66631851
Vasco_Faria <vascomfaria@ua.pt>
2024-05-12 18:54:39 +0100

    appointment add

---
+++ projPet/frontend/javascript/addAppointment.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownItems = document.querySelectorAll(".dropdown-item");
+  
+    dropdownItems.forEach(item => {
+      item.addEventListener("click", function(event) {
+        event.stopPropagation(); 
+        const dropdown = item.querySelector(".dropdown");
+        dropdown.classList.toggle("show");
+      });
+    });
+  
+    window.addEventListener("click", function(event) {
+      dropdownItems.forEach(item => {
+        const dropdown = item.querySelector(".dropdown");
+        if (!item.contains(event.target) && !dropdown.contains(event.target)) {
+          dropdown.classList.remove("show");
+        }
+      });
+    });
+  });
