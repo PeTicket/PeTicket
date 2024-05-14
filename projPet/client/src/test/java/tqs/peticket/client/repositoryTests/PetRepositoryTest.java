31cba4475fd0319e949fa86128b9de80f01e9c74
miragaia <miguelmiragaia2003@gmail.com>
2024-05-14 15:14:26 +0100

    create Service Tests(ProjPet)-PetServiceTest

---
+++ projPet/client/src/test/java/tqs/peticket/client/repositoryTests/PetRepositoryTest.java
@@ -0,0 +1,18 @@
+package tqs.peticket.client.repositoryTests;
+
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import tqs.peticket.client.repository.PetRepository;
+import tqs.peticket.client.service.PetService;
+import tqs.peticket.client.model.Pet;
+
+@DataJpaTest
+public class PetRepositoryTest {
+    
+}
