398ebd425a87f74b5f93e6e330084f1e4c47f364
cristiano-nicolau <cristianonicolau@ua.pt>
2024-05-15 19:24:50 +0100

    havemos papa

---
+++ projPet/client/src/main/java/tqs/peticket/client/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package tqs.peticket.client.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import tqs.peticket.client.model.User;
+import tqs.peticket.client.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(email);
+    if (user == null) {
+      throw new UsernameNotFoundException("User Not Found with email: " + email);
+    }
+    return UserDetailsImpl.build(user);
+  }
+
+}
