a49c29e62c73d4b256f0931aa9d93696b422ecb7
andregomes04 <alg@ua.pt>
2024-05-17 19:48:36 +0100

    Scrum-47: Wanna see sonar

--- projPet/client/src/test/java/tqs/peticket/client/controllerTests/UserControllerTests.java
+++ projPet/client/src/test/java/tqs/peticket/client/controllerTests/UserControllerTests.java
@@ -12,15 +12,20 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
 
 import tqs.peticket.client.controller.UserController;
 import tqs.peticket.client.service.UserService;
 import tqs.peticket.client.serviceTests.Auth;
 import tqs.peticket.client.model.User;
+import tqs.peticket.client.security.jwt.AuthHandler;
 
 
 @WebMvcTest(UserController.class)
@@ -32,6 +37,9 @@ class UserControllerTests {
 
     @MockBean
     private UserService service;
+    
+    @MockBean
+    private AuthHandler authHandler;
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -52,5 +60,35 @@ class UserControllerTests {
         verify(service, times(1)).save(Mockito.any());
 
     }
-    
+
+    @Test
+    void whenSaveUser_ThenUserSaved() throws Exception {
+        User pedro = new User("Pedro", "Lopes", "pdl@deti.com", "abcd1234", "Rua Esquerda, 274", "964434567");
+
+        when(authHandler.getUserId()).thenReturn(UUID.randomUUID());
+
+        when(service.save(any())).thenReturn(null);
+
+        mvc.perform(post("/api/client/user/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtils.toJson(pedro)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void whenDeleteUser_ThenUserNotInDatabase() throws Exception {
+        User pedro = new User("Pedro", "Lopes", "pdl@deti.com", "abcd1234", "Rua Esquerda, 274", "964434567");
+        when(service.save(Mockito.any())).thenReturn(pedro);
+
+        mvc.perform(post("/api/client/user/save")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtils.toJson(pedro)));
+                
+        mvc.perform(delete("/api/client/user/delete"))
+                .andExpect(status().isOk());
+
+        verify(service, times(1)).deleteById(Mockito.any());
+        verify(service, never()).findById(Mockito.any());
+    }
 }
