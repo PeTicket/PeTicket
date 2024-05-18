bd9b44269a59cc23564c7b26bba62212dfdcb5d9
Vasco_Faria <vascomfaria@ua.pt>
2024-05-18 15:27:01 +0100

    login changes

--- projVet/frontend/js/index.js
+++ projVet/frontend/js/index.js
@@ -2,7 +2,7 @@ document.getElementById('loginButton').addEventListener('click', async function(
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('/login', {
+    const response = await fetch('http://localhost:8082/api/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
