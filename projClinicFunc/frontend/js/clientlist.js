535a1098e0d797a2eafe88be67579676e49dd8e9
Vasco_Faria <vascomfaria@ua.pt>
2024-05-25 18:41:14 +0100

    css add pet

--- projClinicFunc/frontend/js/clientlist.js
+++ projClinicFunc/frontend/js/clientlist.js
@@ -1,6 +1,13 @@
 document.getElementById("create-button").addEventListener("click", function() {
     window.location.href = "./manualblockingClient.html"; 
   });
+
+
+  document.getElementById("create-button2").addEventListener("click", function() {
+    window.location.href = "./manualpet.html"; 
+  });
+  
+  
   
   
 
