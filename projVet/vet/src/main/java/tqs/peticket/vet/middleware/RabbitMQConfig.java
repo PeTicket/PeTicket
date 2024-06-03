9278fcad58e8d51f0d409afd9246ced87c452dfd
cristiano-nicolau <cristianonicolau@ua.pt>
2024-06-03 21:07:28 +0100

    commit

--- projVet/vet/src/main/java/tqs/peticket/vet/middleware/RabbitMQConfig.java
+++ projVet/vet/src/main/java/tqs/peticket/vet/middleware/RabbitMQConfig.java
@@ -1,20 +1,48 @@
 package tqs.peticket.vet.middleware;
-
 import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class RabbitMQConfig {
 
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitHost;
+
+    @Value("${spring.rabbitmq.port}")
+    private int rabbitPort;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitUsername;
+
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitPassword;
+
+    @Value("${queue.name}")
+    private String queueName;
+
     @Bean
-    public Queue appointmentQueue(@Value("${queue.name}") String queueName) {
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitHost, rabbitPort);
+        connectionFactory.setUsername(rabbitUsername);
+        connectionFactory.setPassword(rabbitPassword);
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
+
+    @Bean
+    public Queue appointmentQueue() {
         return new Queue(queueName, true);
     }
 
@@ -26,10 +54,5 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue appointmentQueue, DirectExchange directExchange) {
         return BindingBuilder.bind(appointmentQueue).to(directExchange).with("routing-key-teste");
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        return new RabbitTemplate(connectionFactory);
     }
 }
