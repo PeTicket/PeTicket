361edebc7c4485ab9e03cb3e4c9a5a18bd4ed8f1
cristiano-nicolau <cristianonicolau@ua.pt>
2024-06-03 20:13:14 +0100

    commit

--- projClinicFunc/func/src/test/java/tqs/peticket/func/controllerTests/TestSecurityConfig.java
+++ projClinicFunc/func/src/test/java/tqs/peticket/func/controllerTests/TestSecurityConfig.java
@@ -1,22 +1,22 @@
-package tqs.peticket.func.controllerTests;
+// package tqs.peticket.func.controllerTests;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class TestSecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class TestSecurityConfig {
 
-    @SuppressWarnings({ "removal", "deprecation" })
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-            .anyRequest().permitAll();
-        return http.build();
-    }
-}
+//     @SuppressWarnings({ "removal", "deprecation" })
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         http
+//             .csrf().disable()
+//             .authorizeRequests()
+//             .anyRequest().permitAll();
+//         return http.build();
+//     }
+// }
